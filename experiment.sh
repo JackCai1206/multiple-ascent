0b283361d077ed6bb0e83ef7e7ee4f6ab2d4b490
jackcai1206 <jackcai1206@gmail.com>
2024-05-24 08:26:04 -0500

    stuff

--- experiment.sh
+++ experiment.sh
@@ -1,3 +1,11 @@
-for prompt_size in 5 10 25 50 100 200 400 800; do
-    python run.py --prompt_size $prompt_size --out_file="results/num_examples_$num_examples"
+set -e
+
+# for prompt_size in 5 10 25 50 100 200 400 800; do
+for prompt_size in $(seq 5 5 800); do
+    export out_dir="results/num_examples_$prompt_size.csv"
+    if [ -f out_dir ]; then
+        echo "$outdir already exists, skipping"
+        continue
+    fi
+    python run.py --prompt_size $prompt_size --out_file=$out_dir
 done
--- run.py
+++ run.py
@@ -5,11 +5,13 @@ from dataclasses import dataclass, field
 from datasets import Dataset
 import torch
 import pandas as pd
+from tqdm import tqdm
+import numpy as np
 
 @dataclass
 class ScriptArguments:
     model_name: str = "meta-llama/Meta-Llama-3-8B"
-    batch_size: int = 32
+    batch_size: int | None = None
     seed: int = 42
     input_dim: int = 2
     device: str = 'cuda:0'
@@ -32,6 +34,7 @@ def get_dataset_generator(num_examples, model, config):
         x = torch.randint(0, 10, (config.input_dim,)).float()
         y = model(x).int().numpy()[0]
         return x.int().numpy(), y
+    set_seed(config.seed)
     for i in range(num_examples):
         line = ''
         for j in range(config.prompt_size):
@@ -47,7 +50,8 @@ def get_dataset_generator(num_examples, model, config):
 def prompt_model(model: LlamaForCausalLM, tokenizer: LlamaTokenizerFast, prompt: List[str], config: ScriptArguments):
     generation_config = GenerationConfig(max_new_tokens=config.max_new_tokens, num_return_sequences=1)
     inputs = tokenizer(prompt, return_tensors="pt", padding='longest').to(config.device)
-    outputs = model.generate(**inputs, generation_config=generation_config)
+    with torch.no_grad():
+        outputs = model.generate(**inputs, generation_config=generation_config)
     return tokenizer.batch_decode(outputs[:, inputs.input_ids.shape[1]:], skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
 def evaluate(outputs, targets):
@@ -59,6 +63,12 @@ def log_metrics(metrics, df: pd.DataFrame):
             df[key] = [value]
         else:
             df[key]._append(value)
+
+def int_or_nan(x):
+    try:
+        return int(x)
+    except:
+        return np.nan
 
 @torch.no_grad()
 def main():
@@ -73,21 +83,25 @@ def main():
     dataset = Dataset.from_generator(get_dataset_generator, gen_kwargs={'num_examples': config.num_test_examples, 'model': sim_model, 'config': config})
     dataset.set_format(type='torch', columns=['prompt', 'answer'])
 
-    model = AutoModelForCausalLM.from_pretrained(config.model_name, torch_dtype=torch.bfloat16, device_map='cuda:0')
+    model = AutoModelForCausalLM.from_pretrained(config.model_name, torch_dtype=torch.bfloat16, device_map=config.device)
     tokenizer = AutoTokenizer.from_pretrained(config.model_name, padding_side='left')
     tokenizer.pad_token = tokenizer.eos_token
     
     df = pd.DataFrame()
     
-    loss = 0
-    for i in range(max(1, len(dataset) - config.batch_size + 1)):
+    # calculate the batch size
+    if config.batch_size is None: 
+        config.batch_size = max(1, int(40 / (config.prompt_size / 5)))
+    
+    mse = 0
+    for i in tqdm(range(0, len(dataset), config.batch_size), total=int(len(dataset)/config.batch_size)):
         batch = dataset[i:i+config.batch_size]
         outputs = prompt_model(model, tokenizer, batch['prompt'], config)
-        outputs = torch.tensor([int(x) for x in outputs])
+        outputs = torch.tensor([int_or_nan(x) for x in outputs])
         targets = batch['answer']
-        loss += evaluate(outputs, targets)
-    loss /= len(dataset)
-    metrics = {'loss': loss.item()}
+        mse += evaluate(outputs, targets)
+    mse /= len(dataset)
+    metrics = {'mse': mse.item()}
     log_metrics(metrics, df)
 
     df.to_csv(f'{config.out_file}')
